3339cc6892085601e34142e3c61680c25692c17f
笠浪貴大 <dev.kacha@gmail.com>
2026-04-30 23:31:52 +0900

    Hash64作成

---
+++ QuickFind/QuickFind/Hash64.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickFind
+{
+    /// <summary>
+    /// 文字列に使用されている文字の種類で決めたハッシュ値
+    /// ・同じ文字を含む文字列は同じハッシュ値になる。
+    /// ・異なる文字を含む文字列は異なるハッシュ値になる可能性が高い。
+    /// ・コードポイントが小さい文字ほど、1が立つビット位置が小さくなるようにする。
+    /// ・コードポイントが大きい文字ほど、1が立つビット位置が大きくなるようにする。
+    /// 
+    /// 例：
+    /// 記号・・・0x1～2
+    /// 数字・・・0x4～8
+    /// 英字・・・0x10～80
+    /// かな・・・0x400000000等
+    /// 漢字・・・0x4000000000等
+    /// 絵文字・・0x800000000000等
+    /// </summary>
+    public struct Hash64
+    {
+        /// <summary>
+        /// ハッシュ値
+        /// </summary>
+        public UInt64 Value { get; set; } = 0;
+
+        public Hash64(UInt64 value)
+        {
+            Value = value;
+        }
+        public Hash64(string str) : this(str.AsSpan())
+        {
+        }
+        public Hash64(ReadOnlySpan<char> str)
+        {
+            foreach (var rune in str.EnumerateRunes())
+            {
+                Value |= RuneToHash64(rune);
+            }
+        }
+        /// <summary>
+        /// 文字の値をビットフラグに変換する
+        /// </summary>
+        /// <param name="rune">文字</param>
+        /// <returns></returns>
+        public static UInt64 RuneToHash64(Rune rune)
+        {
+            return (UInt64)1 << RuneToShift(rune);
+        }
+        /// <summary>
+        /// コードポイントからシフトする量を計算する
+        /// 例：
+        /// U+0000～U+001F	ASCII	制御文字・・・0を返す
+        /// U+0000～U+007F	ASCII	英数字
+        /// U+0080～U+07FF 各国文字    ラテン文字拡張、ギリシャ文字など
+        /// U+0800～U+FFFF BMP 日本語、中国語、インド系文字など
+        /// U+10000～U+10FFFF 追加面 絵文字、古代文字など
+        /// </summary>
+        public static int RuneToShift(Rune rune)
+        {
+            int codePoint = rune.Value;
+            if (codePoint <= 0x1F)
+            {
+                return 0;
+            }
+            // 先頭のゼロの数を数える
+            // 0b0010_0000	空白文字　・・・26→6
+            // 0x3042 ひらがな　・・・18→14
+            // 0x1F600　絵文字　・・・15→17
+            // 0x10FFFF　　・・・11→21
+            var leadingZeroCount = int.LeadingZeroCount(codePoint);
+            var shift = 32 - leadingZeroCount;
+            if (shift < 6)
+            {
+                return 0;
+            }
+            // 最下位ビット3ビット分残して右シフトする
+            codePoint >>= (shift - 3);
+            // 最上位ビットを0にする
+            codePoint &= 0b11;
+            // 6～21→0～15
+            shift -= 6;
+            // 0～15→0～60
+            shift *= 4;
+            // 0～3の値を加える
+            return shift + codePoint;
+        }
+    }
+}
--- QuickFind/QuickFind/Program.cs
+++ QuickFind/QuickFind/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using QuickFind;
+using System.Text;
+
+//Console.WriteLine("TrailingZeroCount");
+//Console.WriteLine($"{uint.TrailingZeroCount(0b1)}");// 0
+//Console.WriteLine($"{uint.TrailingZeroCount(0b10)}");// 1
+//Console.WriteLine($"{uint.TrailingZeroCount(0b110)}");// 1
+//Console.WriteLine($"{uint.TrailingZeroCount(0b1011)}");// 0
+
+//Console.WriteLine("LeadingZeroCount");
+//Console.WriteLine($"{uint.LeadingZeroCount(0)}");// 32
+//Console.WriteLine($"{uint.LeadingZeroCount(0b1)}");// 31
+//Console.WriteLine($"{uint.LeadingZeroCount(0b10)}");// 30
+//Console.WriteLine($"{uint.LeadingZeroCount(0b110)}");// 29
+//Console.WriteLine($"{uint.LeadingZeroCount(0b1011)}");// 28
+
+#if false
+for (int i = 32; i < 512; i++)
+{
+    Rune rune = new((uint)i);
+    //Console.WriteLine($"{rune}\t{rune.Value:X}\t{Hash64.RuneToShift(rune)}");
+    Console.WriteLine($"{rune}\t{rune.Value:X}\t{Hash64.RuneToHash64(rune):X}");
+}
+#endif
+
+#if false
+string s = " 0Aあいうえおカキクケコ漢😀";
+foreach (var rune in s.EnumerateRunes())
+{
+    //Console.WriteLine($"{rune}\t{rune.Value:X}\t{Hash64.RuneToShift(rune)}");
+    Console.WriteLine($"{rune}\t{rune.Value:X}\t{Hash64.RuneToHash64(rune):X}");
+}
+#endif
+
+var samples = new string[]
+{
+    "!!!!!",
+    "12345",
+    "Hello, World!",
+    "Hash32",
+    "C# 14.0",
+    "ひらがな",
+    "ひらがなカタカナ漢字"
+};
+
+foreach (var sample in samples)
+{
+     var hash = new Hash64(sample);
+     Console.WriteLine($"{sample}→{hash.Value:X}");
+}
